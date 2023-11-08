b4d05c0cac5940c4865da533043b9a8dcbe2e486
Adrian <adrian.george.radu@gmail.com>
2023-11-08 08:38:42 +0100

    Person register/edit views

--- persons/urls.py
+++ persons/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("clients/clients/", views.c_clients, name='c_clients'),
     path("providers/providers/", views.p_providers, name='p_providers'),
+    path("persons/<int:person_id>/", views.person_detail, name='person_detail'),
 ]
--- persons/views.py
+++ persons/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.db.models import Count, Q
 from .models import Person
 from orders.models import Order
 from django.core.paginator import Paginator
+from django.utils import timezone
 
 # Create your views here.
 
@@ -54,3 +55,63 @@ def p_providers(request):
         'providers/p_providers.html',
         {"selected_providers": providers_on_page, "search_name": search_name, "search_service": search_service}
     )
+
+@login_required(login_url='/login/')
+def person_detail(request, person_id):
+    date_now = timezone.now()
+    if person_id != 0:
+        person = get_object_or_404(Person, id=person_id)
+        if request.method == 'POST':
+            update = "Succesfuly updated"
+            person.firstname = request.POST.get('firstname')
+            person.lastname = request.POST.get('lastname')
+            person.entity = request.POST.get('entity')
+            person.gender = request.POST.get('gender')
+            person.identity_card = request.POST.get('identity_card')
+            person.company_name = request.POST.get('company_name')
+            person.company_tax_code = request.POST.get('company_tax_code')
+            person.company_iban = request.POST.get('company_iban')
+            person.email = request.POST.get('email')
+            person.phone = request.POST.get('phone')
+            person.address = request.POST.get('address')
+            person.services = request.POST.get('services')
+            person.modified_at = date_now
+            person.user = request.user
+            person.save()
+        else:
+            update = ""
+    else:
+        if request.method == 'POST':
+            firstname = request.POST.get('firstname')
+            lastname = request.POST.get('lastname')
+            company_name = request.POST.get('company_name')
+            if firstname != "" and lastname != "":
+                try:
+                    person = Person.objects.filter(firstname__icontains=firstname).filter(lastname__icontains=lastname).filter(company_name__icontains=company_name).get()
+                    update = f"Person {person.firstname} {person.lastname} - {person.company_name} exists. Are you sure?"
+                except:
+                    update = "Person created"
+                    person = Person(
+                        firstname = firstname,
+                        lastname = lastname,
+                        company_name = company_name,
+                        entity = request.POST.get('entity'),
+                        gender = request.POST.get('gender'),
+                        identity_card = request.POST.get('identity_card'),
+                        company_tax_code = request.POST.get('company_tax_code'),
+                        company_iban = request.POST.get('company_iban'),
+                        phone = request.POST.get('phone'),
+                        address = request.POST.get('address'),
+                        email = request.POST.get('email'),
+                        services = request.POST.get('services'),
+                        modified_at = date_now,
+                        user = request.user,
+                    )
+                    person.save()
+            else:
+                update = "First name and last name are mandatory."
+                person = ""
+        else:
+            update = ""
+            person = ""
+    return render(request, 'clients/person.html', {'person': person, 'update': update})
