460f815598d5b1514999a11dcd1ee6e53f6e8ef3
Adrian Radu <adrian.george.radu@gmail.com>
2025-03-21 12:34:08 +0100

    Update: user statistics

--- users/views.py
+++ users/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import CustomUser
+from orders.models import Order, Offer
+from persons.models import Person
+from invoices.models import Invoice, Proforma
+from payments.models import Payment
+from appointments.models import Appointment
 from datetime import datetime
 from django.core.paginator import Paginator
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from io import BytesIO
 from PIL import Image
+from datetime import datetime, timedelta
+from django.utils import timezone
 
 import os
 
@@ -122,4 +129,42 @@ def user_detail(request, user_id):
         else:
             update = ""
             user = ""
-    return render(request, "users/user.html", {"us": user, "update": update})
+    def get_stats_by_user(user, flag):
+        now = timezone.now()
+        first_day_this_month = now.replace(day=1)
+        last_month = now.replace(day=1) - timedelta(days=1)
+        first_day_last_month = last_month.replace(day=1)
+
+        def count_query(model, created=True):
+            if flag == "total":
+                return model.objects.filter(created_by=user).count() if created else model.objects.filter(modified_by=user).count()
+            elif flag == "last_month":
+                return model.objects.filter(created_by=user, created_at__range=[first_day_last_month, last_month]).count() if created else model.objects.filter(modified_by=user, modified_at__range=[first_day_last_month, last_month]).count()
+            elif flag == "this_month":
+                return model.objects.filter(created_by=user, created_at__range=[first_day_this_month, now]).count() if created else model.objects.filter(modified_by=user, modified_at__range=[first_day_this_month, now]).count()
+
+        stats = {
+            'persons_reg': count_query(Person, True),
+            'persons_edit': count_query(Person, False),
+            'offers_reg': count_query(Offer, True),
+            'offers_edit': count_query(Offer, False),
+            'orders_reg': count_query(Order, True),
+            'orders_edit': count_query(Order, False),
+            'proformas_reg': count_query(Proforma, True),
+            'proformas_edit': count_query(Proforma, False),
+            'invoices_reg': count_query(Invoice, True),
+            'invoices_edit': count_query(Invoice, False),
+            'payments_reg': count_query(Payment, True),
+            'payments_edit': count_query(Payment, False),
+            'appointments_reg': count_query(Appointment, True),
+            'appointments_edit': count_query(Appointment, False),
+        }
+
+        return stats
+    return render(request, "users/user.html", {
+        "us": user, 
+        "update": update,
+        "total": get_stats_by_user(user, "total"), 
+        "last_month": get_stats_by_user(user, "last_month"),
+        "this_month": get_stats_by_user(user, "this_month"),
+        })
